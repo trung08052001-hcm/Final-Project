dc1142cbd945491848384581cd87d9df6ea23679
Trung <trung03022001@gmail.com>
2023-03-13 17:52:45 +0700

    update code SignUp

--- app/src/main/java/com/example/finalproject/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var login : ImageView = findViewById(R.id.buttonLogin)
+        login.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
