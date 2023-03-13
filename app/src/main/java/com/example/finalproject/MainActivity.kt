ec8c198328d9e568a405716cddd3260624dc9669
Trung <trung03022001@gmail.com>
2023-03-13 13:18:12 +0700

    splash screen

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SplashScreenActivity::class.java)
+            startActivity(intent)
+        }, 5000)
     }
 }
