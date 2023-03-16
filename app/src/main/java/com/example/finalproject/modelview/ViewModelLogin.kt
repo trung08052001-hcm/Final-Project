3d55d98fd9e7e1234f90ceaad35929a55cf347f6
Trung <trung03022001@gmail.com>
2023-03-16 18:23:43 +0700

    viewmodel

---
+++ app/src/main/java/com/example/finalproject/modelview/ViewModelLogin.kt
@@ -0,0 +1,50 @@
+package com.example.finalproject.modelview
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModelLogin : ViewModel() {
+
+    private var _isSuccessEvent: MutableLiveData<Boolean> = MutableLiveData()
+    val isSuccessEvent: LiveData<Boolean>
+        get() = _isSuccessEvent
+
+    private var _isErrorEvent: MutableLiveData<String> = MutableLiveData()
+    val isErrorEvent: LiveData<String>
+        get() = _isErrorEvent
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    fun initSharedPreferences(context: Context) {
+        sharedPreferences = context.getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+    }
+
+    fun checkEmailAndPassword(context: Context, email: String, password: String) {
+        val savedEmail = sharedPreferences.getString("email", "")
+        val savedPassword = sharedPreferences.getString("password", "")
+
+        if (email == savedEmail && password == savedPassword) {
+            _isSuccessEvent.postValue(true)
+        } else {
+            _isErrorEvent.postValue("Invalid login information.")
+        }
+    }
+
+
+
+    private fun isEmailValid(email: String): Boolean {
+        val emailPattern = Patterns.EMAIL_ADDRESS
+        return emailPattern.matcher(email).matches() && email.endsWith("@gmail.com")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        val passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$".toRegex()
+        return passwordPattern.matches(password)
+    }
+}
