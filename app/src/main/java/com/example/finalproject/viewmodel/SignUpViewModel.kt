0dee41ceb0d4bf45b363ed6cebefff17e557e4c8
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2023-03-24 22:35:15 +0700

    code minh thành

---
+++ app/src/main/java/com/example/finalproject/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.finalproject.viewmodel
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class SignUpViewModel: ViewModel() {
+    private var _isSuccessEvent: MutableLiveData<Boolean> = MutableLiveData()
+    val isSuccessEvent: LiveData<Boolean>
+        get() = _isSuccessEvent
+
+    private var _isErrorEvent: MutableLiveData<String> = MutableLiveData()
+    val isErrorEvent: LiveData<String>
+        get() = _isErrorEvent
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    fun initSharedPreferences(context: Context) {
+        sharedPreferences = context.getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+    }
+
+    fun SignUp(context: Context, fullName : String, email: String, password: String) {
+        if (fullName.isEmpty()) {
+            _isErrorEvent.postValue("Please enter your full name")
+            return
+        }
+        if (!isEmailValid(email)) {
+            _isErrorEvent.postValue("Email is invalid")
+            return
+        }
+
+        if (!isPasswordValid(password)) {
+            _isErrorEvent.postValue("Password is invalid")
+            return
+        }
+
+
+        editor.putString("fullName", fullName)
+        editor.putString("email", email)
+        editor.putString("password", password)
+        editor.apply()
+
+        _isSuccessEvent.postValue(true)
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        val emailPattern = Patterns.EMAIL_ADDRESS
+        return emailPattern.matcher(email).matches() && email.endsWith("@gmail.com")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        val passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$".toRegex()
+        return passwordPattern.matches(password)
+    }
+}
