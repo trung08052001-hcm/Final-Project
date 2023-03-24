a15f55c08bcd41b430477a6a68d8156663f48b76
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2023-03-24 22:08:12 +0700

    add code onboarding

---
+++ app/src/main/java/com/example/finalproject/screen/OnboardingScreen/OnboardingScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalproject.screen.OnboardingScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalproject.R
+
+class OnboardingScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
