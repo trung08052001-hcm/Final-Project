0e0a05a31c289e0c7591eebb429e0907ff5d2295
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2023-03-25 19:34:05 +0700

    update develop

--- app/src/main/java/com/example/finalproject/screen/Welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/finalproject/screen/Welcome/WelcomeActivity.kt
@@ -1,7 +1,8 @@
-package com.example.finalproject
+package com.example.finalproject.screen.Welcome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalproject.R
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
