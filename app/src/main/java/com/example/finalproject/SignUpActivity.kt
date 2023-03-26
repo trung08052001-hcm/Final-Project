240dadd47af2ac9ecfc2392cec02f8ee5b4ffa39
Thanh0939744968 <minhthanhlxag5@gmail.com>
2023-03-26 14:04:24 +0700

    update code SignUp and Login

--- app/src/main/java/com/example/finalproject/SignUpActivity.kt
+++ app/src/main/java/com/example/finalproject/SignUpActivity.kt
@@ -1,11 +1,42 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.finalproject.databinding.ActivitySignUpBinding
+import com.example.finalproject.screen.Login.LoginActivity
+import com.example.finalproject.viewmodel.SignUpViewModel
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var viewModel: SignUpViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding.buttonSignUp.setOnClickListener {
+            val fullname = binding.inputnameSignup.text.toString().trim()
+            val email = binding.edtEmailSignUp.text.toString().trim()
+            val password = binding.edtPasswordSignUp.text.toString().trim()
+            val phone = binding.edtPhoneSignUp.text.toString().trim()
+            viewModel.SignUp(this, fullname,email, password)
+        }
+
+
+        viewModel.isSuccessEvent.observe(this, {
+            if (it) {
+
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
+
+        viewModel.isErrorEvent.observe(this, {
+
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        })
     }
 }
