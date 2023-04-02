6696ecbe703ebde4f723b1b7f661bb10dbc06d7d
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2023-04-02 18:23:12 +0700

    update code login google

--- app/src/main/java/com/example/finalproject/WellcomeFragment.kt
+++ app/src/main/java/com/example/finalproject/WellcomeFragment.kt
@@ -1,17 +1,30 @@
 package com.example.finalproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.finalproject.databinding.FragmentWellcomeBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class WellcomeFragment : Fragment() {
     private lateinit var binding: FragmentWellcomeBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val requestCodeGoogleSignIn = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,15 +32,61 @@ class WellcomeFragment : Fragment() {
     ): View? {
 
         binding = FragmentWellcomeBinding.inflate(inflater,container,false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        binding.buttonGoogleLogin.setOnClickListener {
+            signInGoogle()
+        }
+
         return binding.root
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.buttonWelcomeLogin.setOnClickListener {
-            val controller = findNavController()
-            controller.navigate(R.id.action_wellcomeFragment_to_loginFragment)
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, requestCodeGoogleSignIn)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == requestCodeGoogleSignIn && data != null) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleGoogleSignInResult(task)
         }
     }
 
+    private fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account!!)
+        } catch (e: ApiException) {
+            Log.w(TAG, "handleGoogleSignInResult:error", e)
 
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    findNavController().navigate(R.id.action_wellcomeFragment_to_homeFragment)
+                    // TODO: navigate to next screen
+                } else {
+                    Log.w(TAG, "firebaseAuthWithGoogle:error", task.exception)
+                    // TODO: show error message to user
+                }
+            }
+    }
+
+    companion object {
+        private const val TAG = "WellcomeFragment"
+    }
 }
