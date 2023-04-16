c9bb1f03ac95a15480cca897118c5006bde8e46e
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2023-04-16 21:17:24 +0700

    update code

--- app/src/main/java/com/example/finalproject/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/finalproject/viewmodel/ProfileViewModel.kt
@@ -6,17 +6,5 @@ import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
 
-    private lateinit var sharedPreferences: SharedPreferences
 
-    fun initSharedPreferences(context: Context) {
-        sharedPreferences = context.getSharedPreferences("MySharedPreferences", Context.MODE_PRIVATE)
-    }
-
-    fun getFullName(): String {
-        return sharedPreferences.getString("fullName", "") ?: ""
-    }
-
-    fun getEmail(): String {
-        return sharedPreferences.getString("email", "") ?: ""
-    }
 }
